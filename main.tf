e2449f5a80ebece46a4c72148dfc88ac49ded34b
Sharad Vitthal Sanap <126940869+Sharadsanap45@users.noreply.github.com>
2026-09-19 10:34:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
 
+resource "aws_s3_bucket" "practice" {
+  bucket = "sharad-terraform-practice-2026-unique"
+
+  tags = {
+    Name        = "Terraform Practice"
+    Environment = var.environment
+  }
+}
+
+resource "aws_instance" "devops_server" {
+  ami           = "YOUR_AMI_ID"
+  instance_type = var.instance_type
+
+  tags = {
+    Name        = "Terraform-DevOps-Server"
+    Environment = var.environment
+  }
+}
