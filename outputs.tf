8483b28c23dbdd3cfb957ac52f872c7df5412aa5
Sharad Vitthal Sanap <126940869+Sharadsanap45@users.noreply.github.com>
2026-09-19 10:35:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,15 @@
 
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.practice.bucket
+}
+
+output "ec2_instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.devops_server.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.devops_server.public_ip
+}
